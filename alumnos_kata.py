df9a6f362b607051175b8794ef3f7332c6a3fef8
Javier <javier.mura84@gmail.com>
2021-07-26 00:09:44 +0200

    Create alumnos_kata.py

---
+++ alumnos_kata.py
@@ -0,0 +1,41 @@
+class Alumno():
+    Nombre = ''
+    Apellidos = ''
+    Dni = ''
+    Edad = 0
+    Nota = 0
+
+    def __init__(self, nombre, apellidos, dni, edad):
+        self.Nombre = nombre
+        self.Apellidos = apellidos
+        self.Dni = dni
+        self.Edad = edad
+
+    def Saludar(self):
+        print(f'Hola, me llamo {self.Nombre}.')
+
+    def AñadirNota(self, nota):
+        self.Nota
+
+    def CumplirAños(self):
+        self.Edad += 1
+
+jose = Alumno('José', 'Pérez Martínez', '94839294M', 20)
+
+#jose.Nombre = 'José'
+#jose.Apellidos = 'Pérez Martínez'
+#jose.Dni = '94839294M'
+#jose.Edad = 20
+
+jose.Nota = 9
+
+print(jose.Nombre)
+print(jose.Apellidos)
+print(jose.Dni)
+print(jose.Edad)
+print(jose.Nota)
+
+jose.CumplirAños()
+
+print(jose.Edad)
+jose.Saludar()
