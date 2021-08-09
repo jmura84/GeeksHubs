f1868bf0ce5c78cda39332cab152d353c527179b
Javier <javier.mura84@gmail.com>
2021-08-09 20:16:26 +0200

    Update reto_oop.py

---
+++ Reto_4/reto_oop.py
@@ -0,0 +1,86 @@
+class Serie():
+    # Propiedades
+    __titulo = ''
+    __temporadas = 3
+    entregado = False
+    __genero = ''
+    __creador = ''
+
+    # Constructor
+    def __init__(self, titulo, temporadas, genero, creador):
+        self.set_titulo(titulo)
+        self.set_temporadas(temporadas)
+        self.set_genero(genero)
+        self.set_creador(creador)
+    # Get / Set
+    @property
+    def titulo(self):
+        return self.__titulo
+    @titulo.setter
+    def set_titulo(self, titulo):
+        self.__titulo = titulo
+    @property
+    def temporadas(self):
+        return self.__temporadas
+    @temporadas.setter
+    def set_temporadas(self, temporadas):
+        self.__temporadas = temporadas
+    @property
+    def genero(self):
+        return self.__genero
+    @temporadas.setter
+    def set_genero(self, genero):
+        self.__genero = genero
+    @property
+    def creador(self):
+        return self.__creador
+    @creador.setter
+    def set_creador(self, creador):
+        self.__creador = creador
+
+    # Métodos
+
+
+
+class Videojuego():
+    # Propiedades
+    __titulo = ''
+    __horas_estimadas = 10
+    entregado = False
+    __genero = ''
+    __compañia = ''
+
+    def __init__(self, titulo, horas_estimadas, genero, compañia):
+        self.set_titulo(titulo)
+        self.set_horas_estimadas(horas_estimadas)
+        self.set_genero(genero)
+        self.set_compañia(compañia)
+
+    # Get / Set
+    @property
+    def titulo(self):
+        return self.__titulo
+    @titulo.setter
+    def set_titulo(self, titulo):
+        self.__titulo = titulo
+    @property
+    def horas_estimadas(self):
+        return self.__horas_estimadas
+    @horas_estimadas.setter
+    def set_horas_estimadas(self, horas_estimadas):
+        self.__horas_estimadas = horas_estimadas
+    @property
+    def genero(self):
+        return self.__genero
+    @genero.setter
+    def set_genero(self, genero):
+        self.__genero = genero
+    @property
+    def compañia(self):
+        return self.__compañia
+    @compañia.setter
+    def set_compañia(self, compañia):
+        self.__compañia = compañia
+
+    # Métodos
+
