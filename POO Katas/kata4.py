226a813fb261d9409b1743bd1632a343a6197f0f
Javier <javier.mura84@gmail.com>
2021-08-09 02:33:40 +0200

    Update kata4.py

--- POO Katas/kata4.py
+++ POO Katas/kata4.py
@@ -3,7 +3,8 @@ class Alumno():
     nombre = ''
     apellidos = ''
     __dni = ''
-    edad = 0
+    __edad = 0
+    __id = 0
     nota = 0
     asignaturas = []
     _id = 0
@@ -13,16 +14,25 @@ class Alumno():
         self.nombre = nombre
         self.apellidos = apellidos
         self.__dni = dni
-        self.edad = edad
-        self._id = id
+        self.__edad = edad
+        self.__id = id
 
     # Get / Set
     @property
     def id(self):
-        return self._id
+        return self.__id
     @property
     def dni(self):
         return self.__dni
+    @property
+    def edad(self):
+        return self.__edad
+    @edad.setter
+    def edad(self, edad):
+        if edad > 0 and edad <= 100:
+            self.__edad = edad
+        else:
+            raise ValueError('Edad fuera del rango')
 
     # Métodos
     def Saludar(self):
@@ -36,7 +46,7 @@ class Alumno():
             raise ValueError('La nota debe ser 0 o mayor y no puede ser mayor de 10.')
 
     def CumplirAños(self):
-        self.edad += 1
+        self.__edad += 1
 
     def AñadirAsignatura(self, asignatura):
         self.asignaturas.append(asignatura)
@@ -58,38 +68,47 @@ class Asignatura():
 
     # Constructor
     def __init__(self, nombre, nota, id):
-        self.nota(nombre, nota)
+        self.nombre = nombre
+        self.__nota = nota
         self.__id = id
 
-    # Métodos
+    # Get / Set
     @property
     def id(self):
         return self.__id
     @property
     def nota(self):
         return self.__nota
+    @id.setter
+    def id(self, id):
+        self.id = id
     @nota.setter
     def nota(self, nombre, nota):
         if nota >= 0 and nota <= 10:
             self.__nota = nota
-            self.nombre = nombre
-            resultado = list((self.nombre, self.__nota))
-            print(resultado)
         else:
             self.__nota = 0
             raise ValueError('La nota debe ser 0 o mayor y no puede ser mayor de 10.')
 
 
 
-class Clase():
+class Aula():
     # Propiedades
     profesor = ''
     alumnos = []
     asignaturas = []
+    __id = 0
 
     # Constructor
-    def __init__(self, profesor):
+    def __init__(self, profesor, alumno, id):
         self.profesor = profesor
+        self.alumnos = alumno
+        self.__id = id
+
+    # Get / Set
+    @property
+    def id(self):
+        return self.__id
 
     # Métodos
     def AñadirAlumno(self, alumno):
@@ -116,62 +135,33 @@ class Clase():
 
 
 
-jose = Alumno('José', 'Pérez Martínez', '94839294M', 20, 10)
+class Usuario(Aula):
+    # Propiedades
+    trimestre = 0
+    # Constructor
+    def __init__(self, profesor, alumno, id, trimestre):
+        super().__init__(profesor, alumno, id)
+        self.trimestre = trimestre
 
-print(jose.nombre)
-print(jose.apellidos)
-# print(jose.__dni)
-print(jose.edad)
 
-jose.CumplirAños()
 
-print(jose.edad)
 
-jose.Saludar()
+manu = Alumno('Manu', 'Pérez', '1231411M', 12, 1)
 
-jose.nota(9)
-print(f'La nota de {jose.nombre} {jose.apellidos} es {jose.nota}.')
+print(manu.edad)
+print(manu.id)
+print(manu.dni)
 
-jose.AñadirAsignatura('Castellano')
-jose.AñadirAsignatura('Mates')
-jose.AñadirAsignatura('Historia')
-jose.AñadirAsignatura('Geografía')
+manu.edad = 15
 
-print(jose.asignaturas)
-
-jose.EliminarAsignatura('Castellano')
+print(manu.edad)
 
 castellano = Asignatura('Castellano', 9, 1)
 
-clase_a = Clase('Benito López')
+print(castellano.nota)
+print(castellano.id)
 
-clase_a.AñadirAsignatura('Castellano')
-clase_a.AñadirAsignatura('Mates')
-clase_a.AñadirAsignatura('Historia')
-clase_a.AñadirAsignatura('Geografía')
-
-clase_a.AñadirAlumno('Pepe')
-clase_a.AñadirAlumno('Carlos')
-clase_a.AñadirAlumno('María')
-clase_a.AñadirAlumno('Pedro')
-clase_a.AñadirAlumno('Carlos')
-
-
-clase_a.BorrarAsignatura('Mates')
-clase_a.BorrarAlumno('Pepe')
-
-
-print(clase_a.profesor)
-print(clase_a.alumnos, end = ' ')
-print(clase_a.asignaturas)
-
-print('El id de José es ' + str(jose.id) + '.')
-print(f'El id de la asignatura {castellano.nombre} es {str(castellano.id)}.')
-print(f'El DNI de {jose.nombre} es {jose.dni}.')
-
-#jose.nombre = 'José'
-#jose.apellidos = 'Pérez Martínez'
-#jose.dni = '94839294M'
-#jose.edad = 20
-
-
+pepe = Usuario('José', 'Pedro', 2, 3)
+print(pepe.profesor)
+print(pepe.alumnos)
+print(pepe.trimestre)
