a5d15cbd3e8aa5dfc4e6d1d29a9ae96ab052b547
Javier <javier.mura84@gmail.com>
2021-08-13 14:51:52 +0200

    Update reto_oop.py

--- Reto_4/reto_oop.py
+++ Reto_4/reto_oop.py
@@ -1,4 +1,16 @@
+'''
+Este módulo es una práctica de programación orientada a objetos que incluye las clases Serie, Videojuego y Entregable.
+
+'''
+
+__author__ = 'Javier Muñoz Ramón'
+__copyright__ = 'Copyright (c) 2021. All rights are reserved.'
+__license__ = 'GPL 3'
+
 class Serie():
+    '''
+    Clase que define los objetos de tipo serie, sus características y su estado de entrega.
+    '''
     # Propiedades
     __titulo = ''
     __temporadas = 3
@@ -47,6 +59,9 @@ class Serie():
 
 
 class Videojuego():
+    '''
+    Clase que define los objetos de tipo videojuego, sus características y su estado de entrega.
+    '''
     # Propiedades
     __titulo = ''
     __horas_estimadas = 10
@@ -114,21 +129,22 @@ class Entregable():
         else:
             print(f'El artículo {obj1.titulo} está en estado devuelto')
     def compareTo(self, obj1, obj2):
-        if type(obj1) and type(obj2) == Serie:
-            if obj1.temporadas > obj2.temporadas:
-                print(f'{obj1.titulo} tiene {obj1.temporadas - obj2.temporadas} temporadas más que {obj2.titulo}')
-            elif obj1.temporadas < obj2.temporadas:
-                print(f'{obj2.titulo} tiene {obj2.temporadas - obj2.temporadas} temporadas más que {obj1.titulo}')
-            else:
-                print(f'{obj1.titulo} y {obj2.titulo} tienen {obj1.temporadas} temporadas')
-        elif type(obj1) and type(obj2) == Videojuego:
-            if obj1.horas_estimadas > obj2.horas_estimadas:
-                print(f'{obj1.titulo} tiene {obj1.horas_estimadas - obj2.horas_estimadas} horas más de duración que {obj2.titulo}')
-            elif obj1.horas_estimadas < obj2.horas_estimadas:
-                print(f'{obj2.titulo} tiene {obj2.temporadas - obj2.temporadas} horas más de duración que {obj1.titulo}')
-            else:
-                print(f'{obj1.titulo} y {obj2.titulo} tienen {obj1.horas_estimadas} horas de duración')
-        else:
+        try:
+            if type(obj1) and type(obj2) == Serie:
+                if obj1.temporadas > obj2.temporadas:
+                    print(f'{obj1.titulo} tiene {obj1.temporadas - obj2.temporadas} temporadas más que {obj2.titulo}')
+                elif obj1.temporadas < obj2.temporadas:
+                    print(f'{obj2.titulo} tiene {obj2.temporadas - obj2.temporadas} temporadas más que {obj1.titulo}')
+                else:
+                    print(f'{obj1.titulo} y {obj2.titulo} tienen {obj1.temporadas} temporadas')
+            elif type(obj1) and type(obj2) == Videojuego:
+                if obj1.horas_estimadas > obj2.horas_estimadas:
+                    print(f'{obj1.titulo} tiene {obj1.horas_estimadas - obj2.horas_estimadas} horas más de duración que {obj2.titulo}')
+                elif obj1.horas_estimadas < obj2.horas_estimadas:
+                    print(f'{obj2.titulo} tiene {obj2.temporadas - obj2.temporadas} horas más de duración que {obj1.titulo}')
+                else:
+                    print(f'{obj1.titulo} y {obj2.titulo} tienen {obj1.horas_estimadas} horas de duración')
+        except AttributeError:
             print('Solo se pueden comparar si son de la misma categoría: series o videojuegos')
 
 if __name__ == '__main__':
@@ -145,6 +161,9 @@ if __name__ == '__main__':
     # bokunohero.titulo
 
     naruto = Serie('Naruto', 3, 'Anime shonen', 'Masashi Kishimoto')
+    dragonball = Serie('Dragon Ball', 20, 'Anime shonen', 'Akira Toriyama')
+    southpark = Serie('South Park', 26, 'Humor', 'Matt Stone / Trey Parker')
+    mandalorian = Serie('Mandalorian', 1, 'Acción', 'Jon Favreau')
 
     interfaz = Entregable()
 
@@ -162,12 +181,20 @@ if __name__ == '__main__':
     print(guiltygear.entregado)
 
     fighterz = Videojuego('Dragon Ball FighterZ', 150, 'Lucha', 'Arc System Works')
+    f12021 = Videojuego('F1 2021', 400, 'Carreras', 'Codemasters / EA')
+    overwatch = Videojuego('Overwatch', 1000, 'Shoot-em-up', 'Blizzard / Activision')
+    codwarzone = Videojuego('Call of Duty: Warzone', 500, 'Shoot-em-up', 'Blizzard / Activision')
+
+    series = [bokunohero, naruto, dragonball, southpark, mandalorian]
+
+    videojuegos = [guiltygear, fighterz, f12021, overwatch, codwarzone]
 
     # guiltygear.set_titulo('Dragon Ball FighterZ')
     # print(guiltygear.titulo)
 
-    interfaz.compareTo(bokunohero, naruto)
-    interfaz.compareTo(guiltygear, fighterz)
+    interfaz.compareTo(series[0], series[1])
+    interfaz.compareTo(videojuegos[0], videojuegos[1])
+    interfaz.compareTo(series[2], videojuegos[2])
 
     interfaz.devolver(bokunohero)
     print(bokunohero.entregado)
