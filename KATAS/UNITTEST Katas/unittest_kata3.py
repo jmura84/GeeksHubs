d23810af5ab2a653ecaa09adb657669efe5e0e4b
Javier <javier.mura84@gmail.com>
2021-09-10 03:05:19 +0200

    Create unittest_kata3.py

---
+++ KATAS/UNITTEST Katas/unittest_kata3.py
@@ -0,0 +1,31 @@
+import unittest
+
+from distutils.version import StrictVersion
+
+from pint.compat import HAS_NUMPY, HAS_UNCERTAINTIES, NUMPY_VER
+
+
+class TestSkip(unittest.TestCase):
+
+    def requires_numpy18(self):
+        if not HAS_NUMPY:
+            return unittest.skip('Requires Numpy')
+        return unittest.skipUnless(NUMPY_VER) >= StrictVersion('1.8'), 'Requires Numpy >= 1.8'
+
+    def requires_numpy_previous_than(version):
+        if not HAS_NUMPY:
+            return unittest.skip('Requires Numpy')
+        return unittest.skipUnless(NUMPY_VER) < StrictVersion(version), 'Requires Numpy < 1.8'
+
+    def requires_numpy(self):
+        return unittest.skipUnless(HAS_NUMPY, 'Requires Numpy')
+
+    def requires_uncertainties(self):
+        return unittest.skipUnless(HAS_UNCERTAINTIES, 'Requires Uncertainties')
+
+    def requires_not_uncertainties(self):
+        return unittest.skipIf(HAS_UNCERTAINTIES, 'Requires uncertainties is not installed.')
+
+
+if __name__ == '__main__':
+    unittest.main()
