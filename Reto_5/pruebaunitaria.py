6f1aa672698c4eb81095b719b06ac52f8b513d30
Javier <javier.mura84@gmail.com>
2021-09-15 02:14:46 +0200

    Update pruebaunitaria.py

--- Reto_5/pruebaunitaria.py
+++ Reto_5/pruebaunitaria.py
@@ -1,5 +1,5 @@
 import unittest
-import Geometria as geo
+from Geometria.py import Geometria as geo
 
 class TestGeometria(unittest.TestCase):
 
@@ -20,6 +20,46 @@ class TestGeometria(unittest.TestCase):
         """
         result = c.sumar(8, 78)
         self.assertEqual(result, 86)
+        print('tearDown() -> OK')
+
+    def test_areacuadrado(self):
+        result = geo.areaCuadrado(2)
+        self.assertEqual(result, 4)
+        print('tearDown() -> OK')
+
+    def test_areacirculo(self):
+        result = geo.areaCirculo(2)
+        self.assertEqual(result, 12.57)
+        print('tearDown() -> OK')
+
+    def test_areatriangulo(self):
+        result = geo.areaTriangulo(2, 3)
+        self.assertEqual(result, 3)
+        print('tearDown() -> OK')
+
+    def test_arearectangulo(self):
+        result = geo.areaRectangulo(4, 2)
+        self.assertEqual(result, 8)
+        print('tearDown() -> OK')
+
+    def test_areapentagono(self):
+        result = geo.areaPentagono(3, 2)
+        self.assertEqual(result, 3)
+        print('tearDown() -> OK')
+
+    def test_arearombo(self):
+        result = geo.areaRombo(6, 2)
+        self.assertEqual(result, 6)
+        print('tearDown() -> OK')
+
+    def test_arearomboide(self):
+        result = geo.areaRomboide(3, 3)
+        self.assertEqual(result, 9)
+        print('tearDown() -> OK')
+
+    def test_areatrapecio(self):
+        result = geo.areaTrapecio(2, 2, 2)
+        self.assertEqual(result, 2)
         print('tearDown() -> OK')
 
     def tearDown(self):
