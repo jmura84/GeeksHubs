cdda834a1f9fbabe2f58c1bee8cf175652cfdfd7
Javier <javier.mura84@gmail.com>
2021-11-10 00:00:55 +0100

    a

--- KATAS/07_DJANGO Katas/01_Primer_proyecto/mysite/myapi/serializers.py
+++ KATAS/07_DJANGO Katas/01_Primer_proyecto/mysite/myapi/serializers.py
@@ -4,4 +4,5 @@ from .models import Hero
 class HeroSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Hero
-        fields = ('name', 'alias')
+        # id = serializers.ReadOnlyField()
+        fields = ('id', 'name', 'alias')
