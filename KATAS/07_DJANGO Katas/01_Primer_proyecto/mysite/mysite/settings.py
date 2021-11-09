2b3249aff90d9634ecc914f67155872754983891
Javier <javier.mura84@gmail.com>
2021-11-09 13:54:06 +0100

    a

--- KATAS/07_DJANGO Katas/01_Primer_proyecto/mysite/mysite/settings.py
+++ KATAS/07_DJANGO Katas/01_Primer_proyecto/mysite/mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES={
    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
       'NAME':'heroes',
-      'USER':'admin',
+      'USER':'postgres',
       'PASSWORD':'000666000',
       'HOST':'localhost',
       'PORT':'5432',
