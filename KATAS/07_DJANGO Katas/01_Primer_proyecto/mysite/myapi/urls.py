52902b80dffc85ca3f77f8f28589966570ce39f9
Javier <javier.mura84@gmail.com>
2021-11-11 04:00:03 +0100

    a

--- KATAS/07_DJANGO Katas/01_Primer_proyecto/mysite/myapi/urls.py
+++ KATAS/07_DJANGO Katas/01_Primer_proyecto/mysite/myapi/urls.py
@@ -3,8 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r"bookstore", views.AuthorViewSet)
-router.register(r"bookstore", views.BookViewSet)
+router.register(r"bookstore", views.AuthorViewSet, views.BookViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
