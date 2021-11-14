774876758bad262c3abb35032655b25f530ce57e
Javier <javier.mura84@gmail.com>
2021-11-14 21:06:00 +0100

    a

---
+++ Reto_9/mysite/api_bookstore/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Api_Author, Api_Book
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Api_Author
+        fields = ('id', 'name', 'created_date', 'added_by_id')
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Api_Author
+        fields = ('id', 'title', 'description', 'created_date', 'added_by_id', 'author_id')
--- Reto_9/mysite/mysite/settings.py
+++ Reto_9/mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api_bookstore.apps.ApiBookstoreConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
