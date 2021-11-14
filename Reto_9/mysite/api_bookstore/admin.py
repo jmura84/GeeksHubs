0aa9a2d2fec628557c66cb73e2fce6117b552e43
Javier <javier.mura84@gmail.com>
2021-11-14 20:58:45 +0100

    a

--- Reto_9/mysite/api_bookstore/admin.py
+++ Reto_9/mysite/api_bookstore/admin.py
@@ -1,2 +1,5 @@
 from django.contrib import admin
-from .models
+from .models import Api_Author, Api_Book
+
+admin.site.register(Api_Author)
+admin.site.register(Api_Book)
--- Reto_9/mysite/api_bookstore/models.py
+++ Reto_9/mysite/api_bookstore/models.py
@@ -1,12 +1,23 @@
 from django.db import models
-
-class Api_Book(models.Model):
-    title =
-    description =
-    created_date =
-    added_by_id =
-    author_id
-
-
+from django.conf import settings
+from django.utils import timezone
 
 class Api_Author(models.Model):
+    name = models.CharField(max_length=200)
+    added_by_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+class Api_Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    author = models.ForeignKey(Api_Author, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
