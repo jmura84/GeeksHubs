477857a0cccf1d49ee50e43386897d8102a13e8c
Javier <javier.mura84@gmail.com>
2021-11-14 20:45:36 +0100

    a

---
+++ Reto_9/mysite/api_bookstore/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Api_Book(models.Model):
+    title =
+    description =
+    created_date =
+    added_by_id =
+    author_id
+
+
+
+class Api_Author(models.Model):
