f8cf35c62e475c9ebc5901e46fc928f7a08d96b9
Javier <javier.mura84@gmail.com>
2021-11-14 14:58:20 +0100

    a

--- Reto_8/mysite/heroapi/models.py
+++ Reto_8/mysite/heroapi/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Hero(models.Model):
-    # auto_increment_id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 60)
     alias = models.CharField(max_length = 60)
 
